4b07dbc0142b08ed5c445de0b70daa19165e3d6c
aash-ami <aash.amirineni@gmail.com>
2024-06-25 23:04:34 -0400

    - universal footer - navbar transparent

--- js/load-html.js
+++ js/load-html.js
@@ -4,3 +4,11 @@ fetch("/header-menu.html", { method: "GET" })
     }).then(function (data) {
         document.getElementById("headermenu").innerHTML = data;
     });
+
+fetch("/footer.html", { method: "GET" })
+    .then(function (response) {
+        return response.text();
+    }).then(function (data) {
+        document.getElementById("page-footer").innerHTML = data;
+        console.log("footer-->",data);
+    });
